abf2fd67d784a0292ebdcd332e04d5d7486a7cfa
MochamadFW <fadillah550@gmail.com>
2025-03-23 23:03:52 +0700

    :hammer: Hot Fix: Freaking font issue stupid sheet

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        retro: ['"Press Start 2P"', ...fontFamily.sans],
-      },
-      colors: {
-        'retro-pink': '#ff00ff', // Use quotes for hyphenated keys
-        'retro-blue': '#00ffff',
-        'retro-green': '#00ff00',
-        'retro-yellow': '#ffff00',
+        retro: ['var(--font-press-start-2p)', ...fontFamily.sans],
       },
     },
   },
